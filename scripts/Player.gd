afa5fa1a78e2032c1a073f58944e8a4000ee95f5
skeleium123 <ny.tobias04@gmail.com>
2024-11-15 11:11:31 +0100

    Pathfinding

--- scripts/Player.gd
+++ scripts/Player.gd
@@ -7,13 +7,12 @@ extends CharacterBody2D
 @export var sprint_speed = 500
 @onready var barrel = $Barrel
 @onready var sword = $Sword
+@export var swing_duration = 0.1
 
 var direction_to_mouse = Vector2.ZERO
 var angle_to_mouse = 0
 var barrel_length = 40
 var cooldown = false
-var sword_attack_animation_length = 0
-
 var spread_sum = 0
 
 var grabbed = false
@@ -21,11 +20,17 @@ var time_to_nibbling = 0
 var active_timer = 0
 
 var arsenal = {
+	"Sword": {
+		"duration": swing_duration,
+		"damage": 15,
+		"fire_mode": "automatic",
+		"sweep_range": deg_to_rad(70) #Degrees 
+	},
 	"Pistol": {
 		"fire_mode": ["semi", "automatic"],
 		"bullets_per_shot": 1,
 		"fire_rate": 2,
-		"spread": 2, #degrees
+		"spread": 2, #Degrees
 		"bullet_type": "9mm",
 		"bullet_damage": 2
 	},
@@ -33,21 +38,29 @@ var arsenal = {
 		"fire_mode": ["semi", "automatic"],
 		"bullets_per_shot" : 7,
 		"fire_rate": 0.5,
-		"spread": 6, # degrees
+		"spread": 6, # Degrees
 		"bullet_type": "buckshot",
 		"bullet_damage": 1
-	},
-	"Sword": {
-		"lag": 2,
-		"damage": 15,
-		"fire_mode": "automatic"
 	}
 }
 
 var arsenal_keys = arsenal.keys()
-
 var selected_weapon = "Pistol"
 var fire_mode_index = 0
+
+var is_swinging = false 
+var swing_start_angle = deg_to_rad(35)
+var swing_end_angle = deg_to_rad(-35)
+var last_barrel_rotation
+var swing_progress = 0.0
+
+var sweep_range = arsenal["Sword"]["sweep_range"]
+var sword_offset = arsenal["Sword"]["sweep_range"] / 2.0
+
+func _ready():
+	barrel.visible = !barrel.visible
+	sword.visible = !sword.visible
+	pass
 
 func _physics_process(delta):
 	var direction = Vector2(Input.get_axis("left", "right"), Input.get_axis("up", "down")).normalized()
@@ -57,57 +70,48 @@ func _physics_process(delta):
 		velocity = direction * sprint_speed
 	#print("Velocity: " + str(velocity.length()))
 	move_and_slide()
-
-		
-func take_damage(damage : float):
-	health -= damage
-	if health <= 0:
-		game_over()
-	pass
-
-func game_over():
-	queue_free()
-
-func _process(delta):
+	
 	var mouse_position = get_global_mouse_position()
-	
 	direction_to_mouse = (mouse_position - global_position).normalized()
-	
 	angle_to_mouse = (mouse_position - global_position).angle()
+	barrel.rotation = angle_to_mouse	
+	var pivot_distance = 20
+	barrel.position = Vector2(cos(angle_to_mouse), sin(angle_to_mouse)) * pivot_distance
 	
-	barrel.rotation = angle_to_mouse
-	
-	sword.rotation = angle_to_mouse + deg_to_rad(-35) 
-	
-	var clock = 0
-	
-	while clock <= sword_attack_animation_length:
-		clock += delta
-	
+	if is_swinging != true:
+		last_barrel_rotation = barrel.rotation
+		sword.rotation = barrel.rotation + swing_start_angle
+	else:
+		swing(delta, last_barrel_rotation)
+
 	if grabbed:
 		active_timer += delta
-
 		if active_timer >= time_to_nibbling:
 			take_damage(10)
 			print("Ouch!")
-			
 	else:
 		active_timer = 0.0
-	
-	var pivot_distance = 20
-	barrel.position = Vector2(cos(angle_to_mouse), sin(angle_to_mouse)) * pivot_distance
 	
 	if Input.is_action_just_pressed("scroll_up"):
 		var current_index = arsenal_keys.find(selected_weapon)
 		current_index = clampi(current_index + 1, 0, arsenal_keys.size() - 1)
 		selected_weapon = arsenal_keys[current_index]
 		print("Selected weapon: ", selected_weapon)
+		print("Current_index: " + str(current_index))
+		if current_index > 0:
+			barrel.visible = true
+			sword.visible = false
 	elif Input.is_action_just_pressed("scroll_down"):
 		var current_index = arsenal_keys.find(selected_weapon)
 		current_index = clampi(current_index - 1, 0, arsenal_keys.size() - 1)
 		selected_weapon = arsenal_keys[current_index]
 		print("Selected weapon: ", selected_weapon)
-	
+		print("Current_index: " + str(current_index))
+		if current_index < 1:
+			sword.visible = true
+			barrel.visible = false
+		
+		
 	if Input.is_action_just_pressed("switch_firemode"):
 		fire_mode_index = (fire_mode_index + 1) % 2
 		print(fire_mode_index)
@@ -126,7 +130,28 @@ func _process(delta):
 					shoot()
 					await get_tree().create_timer(1.0 / arsenal[selected_weapon]["fire_rate"]).timeout
 					cooldown = false
+		elif selected_weapon == "Sword":
+			if Input.is_action_just_pressed("attack"):
+				is_swinging = true
+				swing_progress = 0.0
+				cooldown = true
+				await get_tree().create_timer(swing_duration).timeout
+				cooldown = false
 
+
+func swing(delta, initial_rotation):
+	swing_progress += delta / swing_duration
+	
+	if swing_progress >= 1.0:
+		is_swinging = false
+		swing_progress = 1.0
+
+	var eased_progress = pow(swing_progress, 3)
+	sword.rotation = lerp(swing_start_angle, swing_end_angle, eased_progress) + initial_rotation
+	print(initial_rotation)
+	
+	if swing_progress >= 1.0:
+		sword.rotation = barrel.rotation + swing_start_angle
 
 func shoot():
 	print(selected_weapon + " fired!")
@@ -147,3 +172,8 @@ func shoot():
 	#print("Sum of spread:" + str(spread_sum))
 	print()
 	pass
+	
+func take_damage(damage : float):
+	health -= damage
+	if health <= 0:
+		queue_free()
--- scripts/Zombie.gd
+++ scripts/Zombie.gd
@@ -1,25 +1,48 @@
 extends CharacterBody2D
 
+@onready var navigation_agent_2D : NavigationAgent2D = $NavigationAgent2D
+@onready var collision_shape_2D = $CollisionShape2D
 
-const SPEED = 300.0
-const JUMP_VELOCITY = -400.0
+@export var player_path = NodePath("Player")
+@export var main_path = NodePath("Main")
 
+var player
+var health = 20
+@export var healths = [20, 40, 60]
+var speed = 100
+@export var speeds = [100, 150, 200]
+
+var matrix = []
+
+var direction
+
+func _ready():
+	var shape = collision_shape_2D.shape
+	
+	#navigation_agent_2D.agent_radius = shape.radius
+	
+	#for y in range(0, 41):
+	#	matrix.append([])
+	#	for x in range(0, 71):
+	#		matrix[y].append(x)
+	player = get_node(player_path)
 
 func _physics_process(delta):
-	# Add the gravity.
-	if not is_on_floor():
-		velocity += get_gravity() * delta
+	if player:
+		navigation_agent_2D.target_position = player.global_position
+		var current_position = global_position
+		var next_path_position = navigation_agent_2D.get_next_path_position()
+		var new_velocity = current_position.direction_to(next_path_position).normalized() * speed
+		
+		if navigation_agent_2D.avoidance_enabled:
+			navigation_agent_2D.set_velocity(new_velocity)
+		else:
+			_on_navigation_agent_2d_velocity_computed(new_velocity)
+		
+		#direction = (player.global_position - global_position).normalized()
+		#velocity = direction * speed
+		move_and_slide()
 
-	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
 
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction = Input.get_axis("ui_left", "ui_right")
-	if direction:
-		velocity.x = direction * SPEED
-	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
-
-	move_and_slide()
+func _on_navigation_agent_2d_velocity_computed(safe_velocity: Vector2) -> void:
+	velocity = safe_velocity
